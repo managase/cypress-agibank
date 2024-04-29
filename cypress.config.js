1f185e4111ae9bd1666bf8f8d8c6d5940ff9783a
managase <nagase@gmail.com>
2024-04-29 15:11:40 -0300

    Update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 5000,
+  requestTimeout: 5000,
+  responseTimeout: 5000,
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
   e2e: {
     baseUrl: 'https://blogdoagi.com.br/',
     setupNodeEvents(on, config) {
